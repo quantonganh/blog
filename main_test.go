52465c107702a9f2bef1ff9bdcd0fe0311114421
Quan Anh Tong <quanta.linux@gmail.com>
2020-12-23 23:08:17 +0700

    fix test

--- main_test.go
+++ main_test.go
@@ -14,68 +14,32 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestListAllPosts(t *testing.T) {
+func TestHandler(t *testing.T) {
+	setUp()
+
 	posts, err := getAllPosts("posts/2019/10/test.md")
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(posts))
 
-	p := posts[0]
-	require.NotNil(t, p)
-	assert.Equal(t, "title", p.Title)
-	assert.Equal(t, "2019-10-02", toISODate(p.Date))
-	assert.Equal(t, "description", p.Description)
-	assert.Equal(t, 1, len(p.Tags))
-	assert.Equal(t, "test", p.Tags[0])
-	assert.Equal(t, template.HTML("<p>content</p>\n"), p.Content)
-}
+	t.Run("getAllPosts", func(t *testing.T) {
+		testGetAllPosts(t, posts)
+	})
 
-func TestHomeHandler(t *testing.T) {
-	router := mux.NewRouter()
-	router.HandleFunc("/", homeHandler)
+	t.Run("homeHandler", func(t *testing.T) {
+		testHomeHandler(t, posts)
+	})
 
-	writer := httptest.NewRecorder()
-	request, err := http.NewRequest("GET", "/", nil)
-	assert.NoError(t, err)
-	router.ServeHTTP(writer, request)
+	t.Run("postHandler", func(t *testing.T) {
+		testPostHandler(t, posts)
+	})
 
-	if writer.Code != http.StatusOK {
-		t.Errorf("Response code is %v", writer.Code)
-	}
-}
+	t.Run("tagHandler", func(t *testing.T) {
+		testTagHandler(t, posts)
+	})
 
-func TestTagsHandler(t *testing.T) {
-	router := mux.NewRouter()
-	router.HandleFunc("/tag/test", tagHandler)
-
-	writer := httptest.NewRecorder()
-	request, err := http.NewRequest("GET", "/tag/test", nil)
-	assert.NoError(t, err)
-	router.ServeHTTP(writer, request)
-
-	if writer.Code != http.StatusOK {
-		t.Errorf("Response code is %v", writer.Code)
-	}
-}
-
-func TestPostHandler(t *testing.T) {
-	router := mux.NewRouter()
-	router.HandleFunc("/{year:20[1-9][0-9]}/{month:0[1-9]|1[012]}/{day:0[1-9]|[12][0-9]|3[01]}/{postName}", postHandler)
-
-	writer := httptest.NewRecorder()
-	request, err := http.NewRequest("GET", "/2019/10/02/test", nil)
-	assert.NoError(t, err)
-	router.ServeHTTP(writer, request)
-
-	if writer.Code != http.StatusOK {
-		t.Errorf("Response code is %v", writer.Code)
-	}
-}
-
-func TestMain(m *testing.M) {
-	setUp()
-	code := m.Run()
-	tearDown()
-	os.Exit(code)
+	t.Cleanup(func() {
+		_ = os.Remove("posts/2019/10/test.md")
+	})
 }
 
 func setUp() {
@@ -95,8 +59,56 @@ content`
 	}
 }
 
-func tearDown() {
-	if err := os.Remove("posts/2019/10/test.md"); err != nil {
-		log.Fatal(err)
+func testGetAllPosts(t *testing.T, posts []*Post) {
+	p := posts[0]
+	require.NotNil(t, p)
+	assert.Equal(t, "title", p.Title)
+	assert.Equal(t, "2019-10-02", toISODate(p.Date))
+	assert.Equal(t, "description", p.Description)
+	assert.Equal(t, 1, len(p.Tags))
+	assert.Equal(t, "test", p.Tags[0])
+	assert.Equal(t, template.HTML("<p>content</p>\n"), p.Content)
+}
+
+func testHomeHandler(t *testing.T, posts []*Post) {
+	router := mux.NewRouter()
+	router.HandleFunc("/", homeHandler(posts))
+
+	writer := httptest.NewRecorder()
+	request, err := http.NewRequest("GET", "/", nil)
+	assert.NoError(t, err)
+	router.ServeHTTP(writer, request)
+
+	if writer.Code != http.StatusOK {
+		t.Errorf("Response code is %v", writer.Code)
 	}
 }
+
+func testPostHandler(t *testing.T, posts []*Post) {
+	router := mux.NewRouter()
+	router.HandleFunc("/{year:20[1-9][0-9]}/{month:0[1-9]|1[012]}/{day:0[1-9]|[12][0-9]|3[01]}/{postName}", postHandler(posts))
+
+	writer := httptest.NewRecorder()
+	request, err := http.NewRequest("GET", "/2019/10/02/test", nil)
+	assert.NoError(t, err)
+	router.ServeHTTP(writer, request)
+
+	if writer.Code != http.StatusOK {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+}
+
+func testTagHandler(t *testing.T, posts []*Post) {
+	router := mux.NewRouter()
+	router.HandleFunc("/tag/test", tagHandler(posts))
+
+	writer := httptest.NewRecorder()
+	request, err := http.NewRequest("GET", "/tag/test", nil)
+	assert.NoError(t, err)
+	router.ServeHTTP(writer, request)
+
+	if writer.Code != http.StatusOK {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+}
+
