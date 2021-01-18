a8ffcb4c0a4918c3ceb1c7a696ae8fa75f35e4cb
Quan Anh Tong <quanta.linux@gmail.com>
2021-01-18 09:21:32 +0700

    add GetAllPosts method into PostService

--- http/rss.go
+++ http/rss.go
@@ -6,48 +6,38 @@ import (
 
 	"github.com/gorilla/feeds"
 	"github.com/pkg/errors"
-
-	"github.com/quantonganh/blog"
-	"github.com/quantonganh/blog/http/mw"
 )
 
-func (s *Server) rssHandler(posts []*blog.Post) mw.ErrHandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) error {
-		scheme := "http"
-		if xForwardedProto := r.Header.Get("X-Forwarded-Proto"); xForwardedProto != "" {
-			scheme = xForwardedProto
-		}
-
-		feed := &feeds.Feed{
-			Title: fmt.Sprintf("%s app", r.Host),
-			Link: &feeds.Link{
-				Href: fmt.Sprintf("%s://%s", scheme, r.Host),
-			},
-		}
-
-		var items []*feeds.Item
-		for _, p := range posts {
-			items = append(items, &feeds.Item{
-				Title: p.Title,
-				Link: &feeds.Link{
-					Href: fmt.Sprintf("%s://%s/%s", scheme, r.Host, p.URI),
-				},
-				Description: p.Description,
-				Created:     p.Date.Time,
-			})
-		}
-		feed.Items = items
-
-		rss, err := feed.ToRss()
-		if err != nil {
-			return errors.Errorf("failed to create RSS: %v", err)
-		}
-
-		_, err = w.Write([]byte(rss))
-		if err != nil {
-			return err
-		}
-
-		return nil
+func (s *Server) rssHandler(w http.ResponseWriter, r *http.Request) error {
+	feed := &feeds.Feed{
+		Title: fmt.Sprintf("%s app", r.Host),
+		Link: &feeds.Link{
+			Href: s.URL(),
+		},
 	}
+
+	var items []*feeds.Item
+	for _, p := range s.PostService.GetAllPosts() {
+		items = append(items, &feeds.Item{
+			Title: p.Title,
+			Link: &feeds.Link{
+				Href: fmt.Sprintf("%s/%s", s.URL(), p.URI),
+			},
+			Description: p.Description,
+			Created:     p.Date.Time,
+		})
+	}
+	feed.Items = items
+
+	rss, err := feed.ToRss()
+	if err != nil {
+		return errors.Errorf("failed to create RSS: %v", err)
+	}
+
+	_, err = w.Write([]byte(rss))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
