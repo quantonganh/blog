80ba1e83aa1e2b54752f7589555f547468341728
Quan Anh Tong <quanta.linux@gmail.com>
2021-01-29 15:15:08 +0700

    speed up post lookup by using map

--- http/tag.go
+++ http/tag.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,14 +9,7 @@ import (
 func (s *Server) tagHandler(w http.ResponseWriter, r *http.Request) *AppError {
 	tag := mux.Vars(r)["tagName"]
 
-	postsByTag, err := s.PostService.GetPostsByTag(tag)
-	if err != nil {
-		return &AppError{
-			Error:   err,
-			Message: fmt.Sprintf("failed to get posts by tag: %s", tag),
-			Code:    http.StatusInternalServerError,
-		}
-	}
+	postsByTag := s.PostService.GetPostsByTag(tag)
 
 	if err := s.Renderer.RenderPosts(w, r, postsByTag); err != nil {
 		return &AppError{
